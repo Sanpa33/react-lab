2a8c73868dea4df6318afed98c7fc85bbea68030
Santino <127140452+Sanpa33@users.noreply.github.com>
2025-12-10 12:34:15 -0300

    importa imagenes en variables

--- props-cats/src/App.jsx
+++ props-cats/src/App.jsx
@@ -1,31 +1,37 @@
 import { useState } from 'react'
 import Contact from './component/Contact'
 
+import MrWhiskerson from './images/mr-whiskerson.png'
+import Fluffykins from './images/fluffykins.png'
+import Felix from './images/felix.png'
+import Pumpkin from './images/pumpkin.png'
+
+
 function App() {
 
   return (
     <>
       <div className="contacts">
         <Contact
-          img="src/images/mr-whiskerson.png"
+          img={MrWhiskerson}
           name="Mr. Whiskerson"
           phone="(212) 555-1234"
           email="mr.whiskaz@catnap.meow"
         />
         <Contact
-          img="src/images/fluffykins.png"
+          img={Fluffykins}  
           name="Fluffykins"
           phone="(212) 555-2345"
           email="fluffy@catnap.meow"
         />
         <Contact
-          img="src/images/felix.png"
+          img={Felix}
           name="Felix"
           phone="(212) 575-2995"
           email="felix@catnap.meow"
         />
         <Contact
-          img="src/images/pumpkin.png"
+          img={Pumpkin}
           name="Pumpkin"
           phone="(212) 175-8495"
           email="pumkin@catnap.meow"
