ee6667ca8ca5f2b40c2d23c1c1c6fdb0a23a3f1f
sanpa <127140452+Sanpa33@users.noreply.github.com>
2025-12-20 21:05:40 -0300

    agrega useEffectStarWars

---
+++ useEffectStarWars/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+export default function App() {
+    const [starWarsData, setStarWarsData] = React.useState({})
+    const [count, setCount] = React.useState(1)
+    
+    
+    React.useEffect(() => {
+        fetch(`https://swapi.info/api/people/${count}`)
+            .then(res => res.json())
+            .then(data => setStarWarsData(data))
+    }, [count])
+    
+    return (
+        <div>
+            <h2>The count is {count}</h2>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Get next character</button>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+        </div>
+    )
+}
